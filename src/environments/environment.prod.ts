e3df32472b036945362e250369b8944013aeb192
RameshTanugula <gnitramesh@gmail.com>
2022-11-24 23:17:51 +0530

    changed prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiserver:'http://sunin.in/api'
+  // apiserver:'http://sunin.in/api'
+   apiserver:'http://morning-tax.us-east-1.elasticbeanstalk.com/',
+ 
 };
